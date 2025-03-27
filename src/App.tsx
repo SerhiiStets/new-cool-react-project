61088f1ca918f13d7136225f67a1d43fd34a22e7
SerhiiStets <stets.serhii@gmail.com>
2025-03-27 13:22:31 +0200

    added new important text

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1>Author: Serhii Stets</h1>
       <p>@ Copyright KPI 2025</p>
     </>
   )
